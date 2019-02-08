106a54e134d57f407d2a5ade13ff92782351870c
Daniel Etter <etter.dt@gmail.com>
2019-02-08 02:02:15 +0000

    Added helpers and tweaked ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,22 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secure_password"
   end
 
-  get "/" do
+  get '/' do
     erb :welcome
+  end
+
+  helpers do
+    def current_user(session)
+      @user = User.find_by_id(session[:user_id])
+    end
+
+    def logged_in?(session)
+      !!current_user(session)
+    end
   end
 
 end
