e2b1ae97b7067be66e82155e17c7eb4e33c62391
Daniel Etter <etter.dt@gmail.com>
2019-02-13 19:18:39 +0000

    Added controller actions for get/post signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,23 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if logged_in?(session)
+      redirect to '/bucket-list-items'
+    else
+      erb :"users/create_user"
+    end
+  end
+
+  post '/signup' do
+    #binding.pry
+    if !params.has_value?("")
+      user = User.create(params)
+      session[:user_id] = user.id
+      redirect '/bucket-list-items'
+    else
+      redirect "/signup"
+    end
+  end
+
+
 end
