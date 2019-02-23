fe6e24b8c145f4f3fc2e88d189f3aa71733dd38c
Daniel Etter <etter.dt@gmail.com>
2019-02-23 02:42:44 +0000

    Added post bucket_list_items route

--- app/controllers/bucket_list_items_controller.rb
+++ app/controllers/bucket_list_items_controller.rb
@@ -1,3 +1,22 @@
 class BucketListItemsController < ApplicationController
+  get '/items/bucket_list_items' do
+    if logged_in?(session)
+      @user = current_user(session)
+      @bucket_list_items = BucketListItem.all
+      erb :"bucket-list-items/bucket-list-items"
+    else
+      redirect "/login"
+    end
+  end
 
+  post '/bucket_list_items' do
+    if !params[:content].empty?
+      user = User.find_by(id: session[:user_id])
+      bucket_list_item = BucketListItem.create(content: params[:content])
+      user.bucket_list_items << bucket_list_item
+      redirect to "/bucket_list_items/#{bucket_list_item.id}"
+    else
+      redirect to "/bucket_list_items/new"
+    end
+  end
 end
