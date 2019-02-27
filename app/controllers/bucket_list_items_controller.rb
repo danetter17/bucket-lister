f4fab86a194e5a0363c8a115d5b1e155a1b9820f
Daniel Etter <etter.dt@gmail.com>
2019-02-27 22:21:42 +0000

    Trying to link bucket list item show pages

--- app/controllers/bucket_list_items_controller.rb
+++ app/controllers/bucket_list_items_controller.rb
@@ -32,9 +32,32 @@ class BucketListItemsController < ApplicationController
   get '/bucket_list_items/:id' do
     if logged_in?(session)
       @bucket_list_item = BucketListItem.find_by(id: params[:id])
-      erb :'/bucket_list_items/show'
+      erb :'bucket_list_items/show_bucket_list_item'
     else
       redirect to "/login"
+    end
+  end
+
+  get '/bucket_list_items/:id/edit' do
+    if logged_in?(session)
+      @bucket_list_item = BucketListItem.find_by(id: params[:id])
+      if @bucket_list_item && @bucket_list_item.user == current_user(session)
+        erb :'/bucket_list_items/edit_bucket_list_item'
+      else
+        redirect '/bucket_list_items'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/bucket_list_items/:id' do
+    @bucket_list_item = BucketListItem.find_by(id: params[:id])
+    if !params[:content].empty?
+      @bucket_list_item.update(content: params[:content])
+      redirect '/bucket_list_items'
+    else
+      redirect "/bucket_list_items/#{@bucket_list_item.id}/edit"
     end
   end
 end
